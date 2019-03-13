fec00fed0e063e1c6ed0bdb462708add41e44117
Liz Davidson <lizdavidson45@gmail.com>
2019-03-12 22:09:25 -0400

    add css style to newbook form

--- book-club-frontend/src/components/NewBook/NewBook.js
+++ book-club-frontend/src/components/NewBook/NewBook.js
@@ -1,55 +1,69 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Axios from "axios";
+import "./NewBook.css";
 
 class NewBook extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            title: "",
-            author: "",
-            image: ""
-        };
-    
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
-    
-      handleChange(event) {
-        this.setState({[event.target.name]: event.target.value});
-      }
-    
-      handleSubmit(event) {
-          Axios.post("http://localhost:3000/api/books", this.state)
-            .then(res => {
-                this.props.getData()
-                this.props.history.push("/")
-            })
-        event.preventDefault();
-      }
-    
-      render() {
-          console.log(this.props)
-        return (
-          <form onSubmit={this.handleSubmit}>
-            <label>
-              Title:
-              <input type="text" name="title" value={this.state.title} onChange={this.handleChange} />
-            </label>
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      author: "",
+      image: ""
+    };
 
-            <label>
-              Author:
-              <input type="text" name="author" value={this.state.author} onChange={this.handleChange} />
-            </label>
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-            <label>
-              Image URL:
-              <input type="text" name="image" value={this.state.image} onChange={this.handleChange} />
-            </label>
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
 
-            <input type="submit" value="Submit" />
-          </form>
-        );
-      }
-    }
-    
+  handleSubmit(event) {
+    Axios.post("http://localhost:3000/api/books", this.state).then(res => {
+      this.props.getData();
+      this.props.history.push("/");
+    });
+    event.preventDefault();
+  }
+
+  render() {
+    console.log(this.props);
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div className="form-content">
+          <label>
+            Title:
+            <input
+              type="text"
+              name="title"
+              value={this.state.title}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Author:
+            <input
+              type="text"
+              name="author"
+              value={this.state.author}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Image URL:
+            <input
+              type="text"
+              name="image"
+              value={this.state.image}
+              onChange={this.handleChange}
+            />
+          </label>
+            <button className="form-button" type="submit">Submit</button>
+        </div>
+      </form>
+    );
+  }
+}
+
 export default NewBook;
