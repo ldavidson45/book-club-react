93561b0973ee0daf0911473c0a12e23e796c64f1
Liz Davidson <lizdavidson45@gmail.com>
2019-03-24 19:59:36 -0400

    add style to book title component

--- book-club-frontend/src/components/Book/BookTitle.js
+++ book-club-frontend/src/components/Book/BookTitle.js
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "./Book.css";
 
 class BookTitle extends Component {
   render() {
     const titles = this.props.books.map(book => {
       return (
-        <li>
-          <Link to="##">{book.title}</Link>;
-        </li>
+        <div
+          className="book-tile"
+          style={{
+            background: `linear-gradient(
+            rgba(0, 0, 0, 0.5),
+            rgba(0, 0, 0, 0.5)
+          ),
+          url(${book.image})`
+          }}
+        >
+          <a className="book-title" href={`/book/${book._id}`}>
+            {book.title}
+          </a>
+        </div>
       );
     });
     return <div>{titles}</div>;
